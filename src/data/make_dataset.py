564e5ad2716450cc8bf9bfbc221f2f07e5ea121e
rayhsu520 <rayhsu520@gmail.com>
2022-07-13 14:57:35 -0400

    Update make_dataset.py

---
+++ src/data/make_dataset.py
@@ -0,0 +1,35 @@
+def load_data(datasetname, column, seq_len, normalise_window):
+    # A support function to help prepare datasets for an RNN/LSTM/GRU
+    data = datasetname.loc[:,column]
+
+    sequence_length = seq_len + 1
+    result = []
+    for index in range(len(data) - sequence_length):
+        result.append(data[index: index + sequence_length])
+    
+    if normalise_window:
+        result = normalise_windows(result)
+
+    result = np.array(result)
+
+    #Last 10% is used for validation test, first 90% for training
+    row = round(0.9 * result.shape[0])
+    train = result[:int(row), :]
+    np.random.shuffle(train)
+    x_train = train[:, :-1]
+    y_train = train[:, -1]
+    x_test = result[int(row):, :-1]
+    y_test = result[int(row):, -1]
+
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))  
+
+    return [x_train, y_train, x_test, y_test]
+
+def normalise_windows(window_data):
+    # A support function to normalize a dataset
+    normalised_data = []
+    for window in window_data:
+        normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+        normalised_data.append(normalised_window)
+    return normalised_data
