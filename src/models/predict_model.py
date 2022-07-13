75c55c8a318b2bb1dde28586369fdf4859c34ee1
rayhsu520 <rayhsu520@gmail.com>
2022-07-13 14:57:55 -0400

    Update predict_model.py

---
+++ src/models/predict_model.py
@@ -0,0 +1,22 @@
+def predict_sequence_full(model, data, window_size):
+    #Shift the window by 1 new prediction each time, re-run predictions on new window
+    curr_frame = data[0]
+    predicted = []
+    for i in range(len(data)):
+        predicted.append(model.predict(curr_frame[newaxis,:,:])[0,0])
+        curr_frame = curr_frame[1:]
+        curr_frame = np.insert(curr_frame, [window_size-1], predicted[-1], axis=0)
+    return predicted
+
+def predict_sequences_multiple(model, data, window_size, prediction_len):
+    #Predict sequence of <prediction_len> steps before shifting prediction run forward by <prediction_len> steps
+    prediction_seqs = []
+    for i in range(int(len(data)/prediction_len)):
+        curr_frame = data[i*prediction_len]
+        predicted = []
+        for j in range(prediction_len):
+            predicted.append(model.predict(curr_frame[newaxis,:,:])[0,0])
+            curr_frame = curr_frame[1:]
+            curr_frame = np.insert(curr_frame, [window_size-1], predicted[-1], axis=0)
+        prediction_seqs.append(predicted)
+    return prediction_seqs
--- src/models/train_model.py
+++
@@ -1,6 +0,0 @@
-#test
-print('test')
-print('test2')
-print('test3')
-
-print('new branch')
